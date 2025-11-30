59080d4c51e521c3e8e84e6a59d3878ceae2a2fe
Leapheng Keam <leaphengkeam@gmail.com>
2025-11-29 22:00:06 -0500

    created a model for the pagination

---
+++ ApplicationCore/Models/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationCore.Models;
+
+public class PagedResult<T>
+{
+    public IEnumerable<T> Data { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalRecords { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalRecords / PageSize);
+}
